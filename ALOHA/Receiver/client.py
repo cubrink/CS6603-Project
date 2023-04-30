313d181e061e8b2bc46ef9f78d00d640eccf0c88
HQ711 <89590911+HQ711@users.noreply.github.com>
2023-04-30 15:57:15 -0500

    client

---
+++ ALOHA/Receiver/client.py
@@ -0,0 +1,34 @@
+import cv2, imutils, socket
+import numpy as np
+import time
+import base64
+
+
+BUFF_SIZE = 65536
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+HOST='127.0.0.1'
+PORT=4321
+socket_address = (HOST,PORT)
+server_socket.bind(socket_address)
+print('Listening at',socket_address)
+_,client_addr = server_socket.recvfrom(BUFF_SIZE)
+print 'Got connection from', client_addr
+
+node0 = "WithPD"
+node1 = "WoutPD"
+node0_pktno = '0'
+node1_pktno = '0'
+node0_data = '0'
+node1_data = '0'
+while True:
+	packet,_ = server_socket.recvfrom(BUFF_SIZE)
+	data = packet.split()
+	if data[0] == '0':
+		node0_pktno = data[1]
+		node0_data = data[2]
+	else:
+		node1_pktno = data[1]
+		node1_data = data[2]
+	print "%5s : node0_pktno = %5s node0_data = %5s  |  %5s : node1_pktno = %5s node1_data = %5s" % (
+                        node0, node0_pktno, node0_data, node1, node1_pktno, node1_data)
