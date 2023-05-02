0ec39aa5cf10a031f8d33504fc7178fca0d706a3
Tx Beagle <jmmbbv@umsystem.edu>
2023-05-02 02:18:40 +0000

    Update tx client

--- ALOHA/Transmitter/client.py
+++ ALOHA/Transmitter/client.py
@@ -1,5 +1,9 @@
+#import cv2, imutils, socket
 import socket
+#import numpy as np
 import time
+import base64
+import Adafruit_DHT
 
 import Adafruit_DHT
 
@@ -8,7 +12,7 @@ def create_socket(buffer_size):
 	client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, buffer_size)
 	return client_socket
 
-def send_handshake(client_socket, host_ip, port)
+def send_handshake(client_socket, host_ip, port):
 	message = b'Hello'
 	client_socket.sendto(message,(host_ip,port))
 
@@ -18,15 +22,14 @@ DHT_PIN = 66 # Pin 66 on BBB, P8_7
 BUFF_SIZE = 65536
 host_ip = '127.0.0.1'
 port = 1234
-
 WARNING_THRESH_SEC = 60
 NS_PER_S = 10**9
 
+use_synthetic_data = False
 
 if __name__ == '__main__':
 	data_fields = 	['timestamp', 'temperature', 'humidity', 'time_of_measurement', 'warning', 'uninitialized']
-	timestamp = time.time_ns()
-
+	
 	host_name = socket.gethostname()
 	client_socket = create_socket(buffer_size=BUFF_SIZE)
 	send_handshake(client_socket, host_ip, port)
@@ -35,8 +38,12 @@ if __name__ == '__main__':
 	prev_data = dict()
 	unintialized = 1
 	while True:
-		time
-		humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+		timestamp = int(time.time() * NS_PER_S)
+		if use_synthetic_data:
+			humidity, temperature = 50, 50
+		else:
+			humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+		
 		if None in [humidity, temperature]:
 			if unintialized:
 				humidity, temperature = 0, 0
@@ -52,11 +59,11 @@ if __name__ == '__main__':
 			time_of_measurement = timestamp
 
 		warning = 0
-		if (timestamp - time_of_measurement) > WARNING_THRESH_SEC * NS_PER_S:
+		if (timestamp - time_of_measurement) > WARNING_THRESH_SEC:
 			warning = 1
 
 		data = ','.join(
-			[timestamp, temperature, humidity, time_of_measurement, warning, unintialized]
+			map(str, [timestamp, temperature, humidity, time_of_measurement, warning, unintialized])
 		)
 
 		client_socket.sendto(data.encode('utf-8'),(host_ip,port))
@@ -68,4 +75,4 @@ if __name__ == '__main__':
 			warning=warning,
 			unintialized=unintialized,
 		)
-		time.sleep(0.5)
+		time.sleep(0.02)
